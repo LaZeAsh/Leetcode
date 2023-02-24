48138e558d9cc82c3cb2bc9a6b68a03d356b4836
LaZeAsh <ayushrgarg@gmail.com>
2023-02-23 19:51:31 -0500

    412. Fizz Buzz

--- Python/412.Fizz-Buzz/main.py
+++ Python/412.Fizz-Buzz/main.py
@@ -1,5 +1,5 @@
 class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
+    def fizzBuzz(self, n: int) -> list[str]:
         fizz_list = list()
         for i in range(n):
             if (i + 1) % 3 == 0 and (i + 1) % 5 == 0:
